38142b2de8989d13e9799614aaafcc08a5076d3b
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-08 20:19:15 +0300

    Customizing admin area

--- listings/admin.py
+++ listings/admin.py
@@ -2,4 +2,16 @@ from django.contrib import admin
 
 from .models import Listing
 
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'seller')
+    list_display_links = ('id', 'title')
+    list_filter = ('seller', )
+    list_editable = ('is_published', )
+    search_fields = ('title', 'description', 'address',
+                     'city', 'state', 'zip_code', 'price')
+    list_per_page = 25
+
+
+admin.site.register(Listing, ListingAdmin)
--- sellers/admin.py
+++ sellers/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Seller
 
-admin.site.register(Seller)
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'hire_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', )
+    list_per_page = 25
+
+
+admin.site.register(Seller, SellerAdmin)
