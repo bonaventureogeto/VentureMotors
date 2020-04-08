b88e0f32a06edfa4c48f696ae13e2b5e67061f64
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-08 18:06:40 +0300

    Created listings and sellers models

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import datetime
+from sellers.models import Seller
 
-# Create your models here.
+class Listing(models.Model):
+    seller = models.ForeignKey(Seller, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    zip_code = models.CharField(max_length=20)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    mileage = models.IntegerField()
+    fuel = models.CharField(max_length=200)
+    body_type = models.CharField(max_length=200)
+    drive_type = models.CharField(max_length=200)
+    engine_size = models.IntegerField()
+    condition = models.CharField(max_length=200)
+    door_count = models.IntegerField()
+    year = models.IntegerField()
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.title
