cc4161a6e1d0caf312742321ad30ab1e9058b148
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-09 08:37:31 +0300

    Adding home and about page dynamic content

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from listings.models import Listing
+from sellers.models import Seller
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)[:3]
+
+    context = {
+        'listings': listings
+    }
+
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    # get all sellers
+    sellers = Seller.objects.order_by('-hire_date')
+
+    # get MVP
+    mvp_sellers = Seller.objects.all().filter(is_mvp=True)
+
+    context = {
+        'sellers': sellers,
+        'mvp_sellers': mvp_sellers
+    }
+
+    return render(request, 'pages/about.html', context)
