c9895fade72bd4ad13044cdf1bd6a0939af44f74
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-23 09:52:38 +0300

    Created accounts app and urls

--- venture/settings.py
+++ venture/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'listings.apps.ListingsConfig',
     'sellers.apps.SellersConfig',
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
