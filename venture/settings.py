5c861a4fbda88323f8477bdc482dad2bd5a3473d
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-04-23 21:20:41 +0300

    Applied changes to settings

--- venture/settings.py
+++ venture/settings.py
@@ -86,7 +86,8 @@ DATABASES = {
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST')
+        'HOST': os.environ.get('DB_HOST'),
+        'CONN_MAX_AGE': 60
     }
 }
 
