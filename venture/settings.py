9956c719edc5fbfb853742cdd9e56c7f17507dba
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-08-27 15:13:44 +0300

    Bug fixes

--- venture/settings.py
+++ venture/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nmsfgsdgfw765329490234bsdq@y83724*&6Jhg%'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['venturemotors.herokuapp.com']
 
@@ -152,8 +152,8 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'bonaogeto1@gmail.com'
-EMAIL_HOST_PASSWORD = 'password'
-EMAIL_USE_TLS = True
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
+EMAIL_PORT = os.environ.get('EMAIL_PORT')
