da6d81fd468e0889bc72c1348088894ca635686e
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-08-27 16:13:15 +0300

    Debug

--- venture/settings.py
+++ venture/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'nmsfgsdgfw765329490234bsdq@y83724*&6Jhg%'
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['venturemotors.herokuapp.com']
 
