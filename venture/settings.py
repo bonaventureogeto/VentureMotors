40c27772d76a54d88d7d661d66462e277c5ffea4
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-08-27 13:26:47 +0300

    Bug fix

--- venture/settings.py
+++ venture/settings.py
@@ -21,13 +21,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'nmsfgsdgfw765329490234bsdq@y83724*&6Jhg%'
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'nmsfgsdgfw765329490234bsdq@y83724*&6Jhg%'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = ['venturemotors.herokuapp.com']
+
 
 print(ALLOWED_HOSTS)
 # Application definition
