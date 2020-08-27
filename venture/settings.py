8369bea5a6845b0c0a81da951ad104c01b6ea463
Bonaventure Ogeto <bonaogeto1@gmail.com>
2020-08-27 16:04:32 +0300

    Bug fixes

--- venture/settings.py
+++ venture/settings.py
@@ -152,8 +152,8 @@ MESSAGE_TAGS = {
 }
 
 # Email config
-EMAIL_HOST = os.environ.get('EMAIL_HOST')
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASSWORD')
-EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
-EMAIL_PORT = os.environ.get('EMAIL_PORT')
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'bonaogeto1@gmail.com'
+EMAIL_HOST_PASSWORD = 'mypassword'
+EMAIL_USE_TLS = True
